61c1847fe22b0f5b364071a5caad589922c5d611
Alexandr Barkowsky <safoktv@gmail.com>
2017-09-21 12:33:04 +0300

    Add new methods in controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,5 +15,24 @@ namespace SPA.Controllers
         {
             return View(repository.GetAll());
         }
+        public ActionResult Add(Reservation item) {
+            if (ModelState.IsValid)
+            {
+                repository.Add(item);
+                return RedirectToAction("Index");
+            }
+            else {
+                return View("Index");
+            }
+        }
+        public ActionResult Update(Reservation item) {
+            if (ModelState.IsValid && repository.Update(item))
+            {
+                return RedirectToAction("Index");
+            }
+            else {
+                return View("Index");
+            }
+        }
     }
 }
